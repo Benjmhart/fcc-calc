73301d9129d38aa1cf7cf4f4bd31097311b0ee70
Ben Hart <benjmhart@gmail.com>
2018-03-11 21:28:54 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ var currTotal=0;
 var chain=[];
 var charCap=8;
 //function to add numbers or decimal by button press
-$('.numbut').click(function(event){
+$('.numbut').unbind('click).bind('click' function(event){
   //get value of button pressed and assign to newNum variable
   event.stopPropagation()
   var newArr=($(this).html()).split('');
